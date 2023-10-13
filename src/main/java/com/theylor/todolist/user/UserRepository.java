8e6235b54792d5cd4164580abb7b0ac8985a23d9
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2023-10-13 18:55:18 -0300

    Criação do Repository e mapeamento do objeto relacional

---
+++ src/main/java/com/theylor/todolist/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.theylor.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<UserModel, UUID>{
+	
+	UserModel findByUsername(String username);
+
+}
